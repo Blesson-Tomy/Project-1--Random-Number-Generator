7624b12522671273596aa64612e2cc320a3aa608
Blesson K Tomy <blessonsmac@gmail.com>
2023-03-05 14:28:59 +0530

    New Project, Python (Backend Update)

---
+++ P1-PYTHON.py
@@ -0,0 +1,11 @@
+import random
+
+Name=str(input("Enter your name:"))
+print("Welcome to RNG",Name,"!")
+
+start=int(input("Enter the Min limit: "))
+end=int(input("Enter the Max limit: "))
+
+num1 = random.randrange(start,end)
+
+print("Your range is from",start,"to",end,"and the number is",num1,".")
